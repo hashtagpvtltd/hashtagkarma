89362f0f9061a896130dfe8d3fb34717705482b3
psykrsna <saivnm5@gmail.com>
2017-12-01 14:58:03 +0530

    Adding config

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
+import config from './config';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 const store = configureStore();
 
-if(process.env.DEPLOYMENT === 'production'){
-  localStorage.setItem('apiRoot', 'http://hashtagkarma.herokuapp.com');
+if(config.DEBUG){
+  localStorage.setItem('apiRoot', 'http://localhost:5000');
 }
 else{
-  localStorage.setItem('apiRoot', 'http://localhost:5000');
+  localStorage.setItem('apiRoot', 'http://hashtagkarma.herokuapp.com');
 }
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
